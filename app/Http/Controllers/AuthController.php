31c3aee6ba3d96fc54f9edf867a77984c09c393b
Masoud <mmanson519@gmail.com>
2025-10-27 21:19:27 +0330

    Auth added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = new User();
+        $user->name =$request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        // ایجاد session و regenerate برای جلوگیری از session fixation
+        $request->session()->regenerate();
+
+        return response()->json(['message' => 'Login successful']);
+    }
+
+    public function me()
+    {
+//        $user = Auth::user();
+
+        return response()->json(Auth::user());
+
+        // فقط اطلاعات مورد نیاز را ارسال کنید
+//        return response()->json($user);
+    }
+    // خروج از حساب کاربری
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout(); // کاربر فعلی را از Auth خارج می‌کند
+
+        // invalidate و regenerate برای امنیت بیشتر
+        session()->invalidate();
+        session()->regenerateToken();
+
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+
+}
